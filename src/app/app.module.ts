e9618dc74cc20daa99f74836fb2638ef8339186f
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-01-20 18:44:36 +0200

    Add initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './parts/header/header.component';
+import { FooterComponent } from './parts/footer/footer.component';
+import { NavComponent } from './parts/nav/nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AdmissionComponent } from './pages/admission/admission.component';
+import { LearningComponent } from './pages/learning/learning.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    AdmissionComponent,
+    LearningComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
