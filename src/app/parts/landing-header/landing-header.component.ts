f423613e9701abede1da91939b9a1cf21f2ce80f
Seif El-Din Sweilam <saifsweelam@gmail.com>
2023-01-31 18:54:44 +0200

    Implement landing header component

--- src/app/parts/landing-header/landing-header.component.ts
+++ src/app/parts/landing-header/landing-header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-header',
   templateUrl: './landing-header.component.html',
   styleUrls: ['./landing-header.component.scss']
 })
-export class LandingHeaderComponent {
 
+export class LandingHeaderComponent implements OnInit {
+  constructor() {}
+
+  @ViewChild('navComponent') navElementRef : any;
+
+  @HostListener('window:scroll', ['$event'])
+  scrollFunction() {
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+      this.navElementRef.nativeElement.classList.remove("transparent");
+    } else {
+      this.navElementRef.nativeElement.classList.add("transparent");
+    }
+  }
+
+  ngOnInit(): void {
+  }
+  
+  // ngAfterViewInit() {
+  //   console.log(this.navElementRef);
+  //   console.log(this.navElementRef.nativeElement);
+  //   window.onscroll = this.scrollFunction;
+  // }
 }
